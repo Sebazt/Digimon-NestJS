9058359a9a8317351f22c2f24e93512089f19adf
Sebazt <juan.londono29@udea.edu.co>
2022-07-16 23:14:33 -0500

    backend done

--- src/digimon/digimon.controller.ts
+++ src/digimon/digimon.controller.ts
@@ -1,28 +1,41 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { DigimonService } from "./digimon.service";
 
 @Controller('DigiCards')
 export class DigimonController {
-  constructor(private readonly digiService: DigimonService){}
+  constructor(private readonly digiService: DigimonService) {}
 
   @Get()
-  getAllDigimons(){
+  getAllDigimons() {
     return this.digiService.getDigimons();
   }
 
   @Get(':digimonId')
-  getDigimon(@Param('digimonId') digimonId:string){
+  getDigimon(@Param('digimonId') digimonId: string) {
     return this.digiService.getDigimonById(digimonId);
   }
 
   @Post()
   createDigimon(
-    @Body('name') name:string,
-    @Body('level') level:string,
-  ){
+    @Body('name') name: string, 
+    @Body('level') level: string) {
     const digimonId = this.digiService.insertdigimon(name, level);
     return {
       id: digimonId,
-    }
+    };
+  }
+
+  @Put(':digimonId')
+  updateDigimon(
+    @Param('digimonId') digimonId: string,
+    @Body('name') name:string,
+    @Body('level') level:string) {
+    return this.digiService.update(digimonId,name,level);
+  }
+
+  @Delete(':digimonId')
+  deleteDigimon(
+    @Param('digimonId') digimonId: string) {
+    return this.digiService.deleteDigimonbyId(digimonId);
   }
 }
--- src/digimon/digimon.service.ts
+++ src/digimon/digimon.service.ts
@@ -3,24 +3,41 @@ import { Digimon } from "./digimon.model";
 import {v4 as uuidv4} from 'uuid'; // librería para generar un id random
 
 @Injectable()
-export class DigimonService{
+export class DigimonService {
   private digimons: Digimon[] = [];
 
-  insertdigimon(name:string, level:string){
-    const id = uuidv4()
+  insertdigimon(name: string, level: string) {
+    const id = uuidv4();
     console.log(id);
-    const newDigimon = new Digimon(id,name,level);
+    const newDigimon = new Digimon(id, name, level);
     this.digimons.push(newDigimon);
     return newDigimon;
   }
 
-  getDigimons(){
+  getDigimons() {
     return [...this.digimons];
   }
 
-  getDigimonById(id:string){
-    return this.digimons.find((digi)=> digi.id === id)
+  getDigimonById(id: string) {
+    return this.digimons.find((digi) => digi.id === id);
   }
 
+  update(id: string, name: string, level: string) {
+    const [targetDigimon, index] = this.getDigiById(id);
+    const newD = { ...targetDigimon, name, level };
+    const newDigi = new Digimon(id, newD.name, newD.level);
+    this.digimons[index] = newDigi;
+    return newDigi;
+  }
 
+  deleteDigimonbyId(id: string) {
+    const [, index] = this.getDigiById(id);
+    this.digimons.splice(index, 1)
+    return this.digimons
+  }
+
+  private getDigiById(id: string): [Digimon, number] {
+    const index = this.digimons.findIndex((x) => x.id === id);
+    return [this.digimons[index], index];
+  }
 }
